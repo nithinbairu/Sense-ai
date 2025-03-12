a435660fd372406735e43423b40cadcbddc63da5
nithinbairu <nithinbairu08@gmail.com>
2025-03-12 23:25:19 +0530

    Landing Page Finished

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/header"
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import HeroSection from "@/components/hero";
 
 const inter=Inter({subsets:["latin"]});
 
@@ -37,8 +38,7 @@ export default function RootLayout({ children }) {
               </div>
             </footer>
           </ThemeProvider>
-        </body>
-        
+        </body> 
       </html>
       </ClerkProvider>
   );
