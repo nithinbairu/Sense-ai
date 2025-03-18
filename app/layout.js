82c4355ea4be4226b57a3a978e87d53734568896
nithinbairu <nithinbairu08@gmail.com>
2025-03-18 12:29:00 +0530

    Routing in Next JS Onboarding Page Onboarding APIs User Onboarding Form Custom hook for Fetching APIs Gemini AI API setup Generation AI Industry Insights

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import Header from "@/components/header"
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import HeroSection from "@/components/hero";
+import { Toaster } from "sonner";
 
 const inter=Inter({subsets:["latin"]});
 
@@ -30,11 +31,12 @@ export default function RootLayout({ children }) {
             {/* <Header></Header> */}
             <Header />
             <main className="min-h-screen">{children}</main>
+            <Toaster richColors/>
             
 
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made with 💗 by RoadsideCoder</p>
+                <p>Made with 💗 by Nithin</p>
               </div>
             </footer>
           </ThemeProvider>
